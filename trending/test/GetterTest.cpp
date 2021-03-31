eee5a26cb40b6ccd9862ba1c7d34367e4c7b073e
Yeolar <yeolar@qq.com>
2021-03-31 23:16:23 +0800

    add comments

--- trending/test/GetterTest.cpp
+++ trending/test/GetterTest.cpp
@@ -11,15 +11,15 @@ using namespace trending;
 TEST(Getter, getLatest) {
   std::vector<double> v8 = {1,2,3,4,5,6,7,8};
   {
-    auto v = getLatest(v8, 0, 8);
+    auto v = getLatest(v8, 0, 8);   // 1,2,3,4,5,6,7,8
     EXPECT_DOUBLE_EQ(4.5, avg(v));
   }
   {
-    auto v = getLatest(v8, 3, 8);
+    auto v = getLatest(v8, 3, 8);   // 1,2,3,4,5
     EXPECT_DOUBLE_EQ(3.0, avg(v));
   }
   {
-    auto v = getLatest(v8, 3, 3);
+    auto v = getLatest(v8, 3, 3);   // 3,4,5
     EXPECT_DOUBLE_EQ(4.0, avg(v));
   }
 }
